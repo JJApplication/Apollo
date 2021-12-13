552a74ed4c2107471bde1963ab5190ad923f3e5d
landers1037 <liaorenj@gmail.com>
2021-12-14 03:27:32 +0800

    update 12.14 jjservice支持使用envcutst配置端口

--- conf/manager/JJService/start.sh
+++ conf/manager/JJService/start.sh
@@ -5,6 +5,10 @@ if [[ ! -d "${APP_LOG}/${APP}" ]];then
   mkdir -p "${APP_LOG}/${APP}"
 fi
 
+conf="${APP_ROOT}/${APP}/conf/production.ini"
+conf_temp="${APP_ROOT}/${APP}/conf/production.ini.env"
+envsubst < "$conf_temp" > "$conf"
+
 nohup  "${APP_ROOT}/${APP}/app_jjservice" > "${APP_LOG}/${APP}/${APP}.log" 2>&1 &
 result=$?
 if [[ $result != 0 ]];then
