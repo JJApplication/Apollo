995c41b75f4366c57cbbbdd8cf1c4bb08efd12df
landers <liaorenj@gmail.com>
2022-07-30 01:43:15 +0800

    feat：增加心跳接口

--- router/router_web/init.go
+++ router/router_web/init.go
@@ -7,6 +7,7 @@ package router_web
 
 import (
 	_ "github.com/JJApplication/Apollo/docs"
+	"github.com/JJApplication/Apollo/router"
 	"github.com/gin-gonic/gin"
 	ginSwagger "github.com/swaggo/gin-swagger"
 	"github.com/swaggo/gin-swagger/swaggerFiles"
@@ -19,5 +20,8 @@ func Init(r *gin.Engine) {
 	{
 		routerWeb.GET("/", Index)
 		routerWeb.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+		routerWeb.GET("heartbeat", func(c *gin.Context) {
+			router.Response(c, "", true)
+		})
 	}
 }
